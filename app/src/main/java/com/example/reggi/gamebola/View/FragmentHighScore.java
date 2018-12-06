135281b82f52d80da616953104611c9c99734e26
Billy Setiadi <billygj616@gmail.com>
2018-12-06 22:52:45 +0700

    perubahan tampilan high score

--- app/src/main/java/com/example/reggi/gamebola/View/FragmentHighScore.java
+++ app/src/main/java/com/example/reggi/gamebola/View/FragmentHighScore.java
@@ -1,12 +1,17 @@
 package com.example.reggi.gamebola.View;
 
 
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.reggi.gamebola.R;
 
@@ -17,6 +22,7 @@ public class FragmentHighScore extends Fragment {
     protected ListenerHighScore listener;
     protected ListView listView;
     protected Adapter adapter;
+    protected TextView tvHighScore,tvRank,tvScore;
 
 
     public FragmentHighScore() {
@@ -30,11 +36,22 @@ public class FragmentHighScore extends Fragment {
     }
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View result = inflater.inflate(R.layout.fragment_high_score,null);
+        this.tvHighScore=result.findViewById(R.id.showSkor);
+        this.tvRank=result.findViewById(R.id.rank);
+        this.tvScore=result.findViewById(R.id.score);
+        Typeface myCustomeFont= null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            myCustomeFont = getResources().getFont(R.font.fipps_regular);
+        }
+        this.tvHighScore.setTypeface(myCustomeFont);
+        this.tvRank.setTypeface(myCustomeFont);
+        this.tvScore.setTypeface(myCustomeFont);
         this.listView = result.findViewById(R.id.listView);
         this.listView.setAdapter(adapter);
         return result;
